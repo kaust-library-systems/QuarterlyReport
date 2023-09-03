12c790fd236b25e71e9961735fa7a53e829a92af
Marcelo Garcia <marcelomgarcia@gmail.com>
2023-09-03 17:28:12 +0300

    click path as python path

--- qreport.py
+++ qreport.py
@@ -1,18 +1,20 @@
 import click as ck
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import pathlib as pl
 
 ck.command()
 ck.argument('input', 
     type=ck.Path(
         exists=True,
         file_okay=True,
-        readable=True))
+        readable=True,
+        path_type=pl.Path))
 def qreport(input):
     """
     qreport generates a graph from the input CSV file
     """
+
 
 if __name__ == "__main__":
     qreport()
